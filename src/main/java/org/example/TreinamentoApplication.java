7424bd81f777d01cb7b9f3b9931501a5bc13406f
mayhmemo <mayhm.social@proton.me>
2026-03-05 11:52:48 -0300

    chore: apply springboot

---
+++ src/main/java/org/example/TreinamentoApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TreinamentoApplication.class, args);
+    }
+}
